3dbab59af5df3d7041c1c962a5a988e04ef13aa9
mahbubur1 <mdmahbuborr462@gail.com>
2022-05-26 14:52:51 +0600

    final commit

--- src/Components/firebase.init.js
+++ src/Components/firebase.init.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyDyFSxR4QUY7MaY1xQZe03fFyqfti1Qv5E",
   authDomain: "halal-food-f3299.firebaseapp.com",
